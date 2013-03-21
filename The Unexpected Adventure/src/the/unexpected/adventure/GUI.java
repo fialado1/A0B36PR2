f27fb4a891bbf734a2321ce3d479763929fba369
Dominik <Dominik@147.32.218.75>
2013-03-21 10:11:14 +0100

    Třídu jsem obohatil o další metody umožnňující  zobrazení menu pro tvorbu nové postavy.

--- The Unexpected Adventure/src/the/unexpected/adventure/GUI.java
+++ The Unexpected Adventure/src/the/unexpected/adventure/GUI.java
@@ -20,22 +20,23 @@ public final class GUI extends JFrame implements Runnable, MouseListener, MouseM
     private Graphics grp;
     private Image[] imgDoorAnim = new Image[12];
     private Image[] imgStartMenu = new Image[4];
-    private int[] widthImgStartM = new int[4];
-    private int[] heightImgStartM = new int[4];
-    private int[] coordXImgStartM = new int[4];
-    private int[] coordYImgStartM = new int[4];
+    private Image[] imgDialogs = new Image[4];
+    private Image[] imgNumbers = new Image[10];
+    private Image[] imgButtons = new Image[6];
+    private Image imgNewGame;
     private int widthScreen;
     private int heightScreen;
     private int fazeHry = 1;
     private int mX;
     private int mY;
-    private int startM;
+    private int menuMouseLoc;
+    Game game = new Game();
 
-    public GUI() {
+    public GUI() throws InterruptedException {
         creatAndShowGUI();
     }
 
-    public void creatAndShowGUI() {
+    public void creatAndShowGUI() throws InterruptedException {
 
         vlakno.start();
 
@@ -47,9 +48,17 @@ public final class GUI extends JFrame implements Runnable, MouseListener, MouseM
         }
         for (int j = 0; j < imgStartMenu.length; j++) {
             imgStartMenu[j] = loadImage("startMenu/door_0_" + j + ".jpg").getImage();
-            widthImgStartM[j] = imgStartMenu[j].getWidth(this);
-            heightImgStartM[j] = imgStartMenu[j].getHeight(this);
         }
+        for (int j = 0; j < imgDialogs.length; j++) {
+            imgDialogs[j] = loadImage("dialogs/quit_" + j + ".jpg").getImage();
+        }
+        for (int j = 0; j < imgNumbers.length; j++) {
+            imgNumbers[j] = loadImage("newGameMenu/" + j + ".png").getImage();
+        }
+        for (int j = 0; j < imgButtons.length; j++) {
+            imgButtons[j] = loadImage("newGameMenu/button" + j + ".png").getImage();
+        }
+        imgNewGame = loadImage("newGameMenu/menu.jpg").getImage();
 
         addMouseMotionListener(this);
         addMouseListener(this);
@@ -63,7 +72,7 @@ public final class GUI extends JFrame implements Runnable, MouseListener, MouseM
 
 
         setTitle("The Unexpected Journey");
-        setIconImage(loadImage("icon/adventura.ico").getImage());
+        setIconImage(loadImage("icon/adventura.png").getImage());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         this.setUndecorated(true);
@@ -89,20 +98,50 @@ public final class GUI extends JFrame implements Runnable, MouseListener, MouseM
         }
     }
 
+    public void quitDialog(Graphics g) {
+        g.drawImage(imgDialogs[menuMouseLoc], (widthScreen - 522) / 2, (heightScreen - 200) / 2, 522, 200, this);
+    }
+
     public void startMenu(Graphics g) {
-        g.drawImage(imgStartMenu[startM], 0, 0, widthScreen, heightScreen, this);
+        g.drawImage(imgStartMenu[menuMouseLoc], 0, 0, widthScreen, heightScreen, this);
 
     }
-    public void loadMenu(Graphics g){
+
+    public void newGameMenu(Graphics g) {
+        doorAnim(g);
+        g.drawImage(imgNewGame, 0, 0, widthScreen, heightScreen, this);
+//        int moveX = 0;
+//        int moveY = 0;
+//        for (; moveX >= 1600 || moveY >= 900; moveX += 16, moveY += 9) {
+//             System.out.println("bsf");
+//            g.drawImage(imgNewGame,  widthScreen/2 - moveX/2, heightScreen/2 - moveY/2,moveX, moveY, this);
+//            System.out.println("wS " + (widthScreen/2 - moveX/2) + "\nhS " +( heightScreen/2 - moveY/2));
+//        }
+        fazeHry = 3;
+    }
+
+    public void newGameMenu2(Graphics g) {
+        g.drawImage(imgButtons[0], 115, 35, 230, 100, this);
+        g.drawImage(imgButtons[1], (widthScreen - 435), (heightScreen - 110), 200, 90, this);
+        int pomocna = 0;
+        for (int i = 0; i < 4; i++) {
+            g.drawImage(imgButtons[4], widthScreen - (130), heightScreen - (556 - pomocna), 58, 29, this);
+            g.drawImage(imgButtons[2], widthScreen - (130), heightScreen - (556 - 33 - pomocna), 58, 29, this);
+            g.drawImage(imgNumbers[5], widthScreen - (130+17+57), heightScreen - (556 - pomocna), 35, 47, this);
+            pomocna += 107;
+        }
+    }
+
+    public void loadMenu(Graphics g) {
         g.drawImage(imgDoorAnim[0], 0, 0, widthScreen, heightScreen, this);
-        
+
     }
 
     public void doorAnim(Graphics g) {
         for (int j = 0; j < imgDoorAnim.length; j++) {
             g.drawImage(imgDoorAnim[j], 0, 0, widthScreen, heightScreen, this);
             try {
-                vlakno.sleep(50);
+                vlakno.sleep(65);
             } catch (InterruptedException ex) {
                 Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -111,34 +150,43 @@ public final class GUI extends JFrame implements Runnable, MouseListener, MouseM
             if ((j + 1) == imgDoorAnim.length) {
                 int moveX = 0;
                 int moveY = 0;
-                for (; moveX <= 4000 || moveY <= 4000; moveX += 100, moveY += 80) {
+                for (; moveX <= 5000 || moveY <= 5000; moveX += 350, moveY += 80) {
                     g.drawImage(imgDoorAnim[j], -moveX, -moveY, widthScreen + (2 * moveX), heightScreen + (2 * moveY), this);
                 }
             }
         }
+        try {
+            vlakno.sleep(500);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
         fazeHry = 2;
+    }
+
+    public void mainGame(Graphics g) {
     }
 
     @Override
     public void paint(Graphics g) {
         switch (fazeHry) {
             case 0://ukonceni hry - vyskoci tabulka jestli si je hrac jisty nebo podekovani mu za zahrani si
-                System.exit(1);
+                quitDialog(g);
                 break;
             case 1://uvodni menu - new, load, quit adventure
                 startMenu(g);
                 break;
             case 2://nova hra - vytvoreni nove postavy
+                newGameMenu(g);
                 break;
-            case 3://tabulka s moznosti naloudovani ulozene hry
+            case 3://
+                newGameMenu2(g);
+                break;
+            case 4://tabulka s moznosti naloudovani ulozene hry
                 loadMenu(g);
                 break;
-            case 4://animace otevreni dveri (spusti se pri kliknuti na novou hru a i po spusteni hry po zvoleni ulozene hry)
-                doorAnim(g);
+            case 5://vlastni hra
+                mainGame(g);
                 break;
-            case 5:
-                break;
-
         }
     }
 
@@ -168,15 +216,24 @@ public final class GUI extends JFrame implements Runnable, MouseListener, MouseM
         mX = e.getX();
         mY = e.getY();
         switch (fazeHry) {
+            case 0:
+                if (mX >= 530 && mX <= 580 && mY >= 410 && mY <= 440) {
+                    menuMouseLoc = 1;
+                } else if (mX >= 825 && mX <= 870 && mY >= 410 && mY <= 440) {
+                    menuMouseLoc = 2;
+                } else {
+                    menuMouseLoc = 0;
+                }
+                break;
             case 1:
                 if (mX >= 625 && mX <= 660 && mY >= 207 && mY <= 350) {
-                    startM = 1;
+                    menuMouseLoc = 1;
                 } else if (mX >= 665 && mX <= 700 && mY >= 245 && mY <= 350) {
-                    startM = 2;
+                    menuMouseLoc = 2;
                 } else if (mX >= 712 && mX <= 745 && mY >= 207 && mY <= 350) {
-                    startM = 3;
+                    menuMouseLoc = 3;
                 } else {
-                    startM = 0;
+                    menuMouseLoc = 0;
                 }
                 break;
         }
@@ -187,20 +244,27 @@ public final class GUI extends JFrame implements Runnable, MouseListener, MouseM
         mX = e.getX();
         mY = e.getY();
         switch (fazeHry) {
+            case 0:
+                if (mX >= 530 && mX <= 580 && mY >= 410 && mY <= 440) {
+                    System.exit(1);
+                } else if (mX >= 825 && mX <= 870 && mY >= 410 && mY <= 440) {
+                    fazeHry = 1;
+                }
+                break;
             case 1:
                 if (mX >= 625 && mX <= 660 && mY >= 207 && mY <= 350) {
-                    fazeHry = 3;
-                } else if (mX >= 665 && mX <= 700 && mY >= 245 && mY <= 350) {
                     fazeHry = 4;
+                } else if (mX >= 665 && mX <= 700 && mY >= 245 && mY <= 350) {
+                    fazeHry = 2;
                 } else if (mX >= 712 && mX <= 745 && mY >= 207 && mY <= 350) {
                     fazeHry = 0;
                 }
                 break;
             case 2:
-                fazeHry = 1;
+
                 break;
             case 3:
-                fazeHry = 1;
+                System.out.println(e.getX() + "    " + e.getY());
                 break;
         }
     }
